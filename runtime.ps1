f8c50b0459a4a1af9ce2852763cf0a529e70aa0e
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2024-07-26 22:17:31 +0300

    Update runtime.ps1

--- runtime.ps1
+++ runtime.ps1
@@ -1,8 +1,4 @@
-If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
-	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
-	Exit
-}
-
+Set-ExecutionPolicy Bypass -Scope Process -Force
 function Install-Chocolatey {
 	# Check if Chocolatey is installed
 	if (-not (Get-Command choco -ErrorAction SilentlyContinue)) {
