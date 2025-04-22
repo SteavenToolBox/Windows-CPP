84ae08edc46b9159070cc97b6d69c5f3f7475a49
SteavenGamerYT <omarhanykasban706@gmail.com>
2025-04-22 22:08:16 +0200

    Update repairdevices.cpp
    now every single disk gets repaired
    anddefragment doesn't work on red hat

--- repairdevices.cpp
+++ repairdevices.cpp
@@ -22,17 +22,11 @@ void RepairDrives() {
 
     // Initialize security.
     hres = CoInitializeSecurity(
-        NULL,
-        -1,                          // COM authentication
-        NULL,                        // Authentication services
-        NULL,                        // Reserved
-        RPC_C_AUTHN_LEVEL_DEFAULT,   // Default authentication 
-        RPC_C_IMP_LEVEL_IMPERSONATE, // Default Impersonation
-        NULL,                        // Authentication info
-        EOAC_NONE,                   // Additional capabilities 
-        NULL                         // Reserved
+        NULL, -1, NULL, NULL,
+        RPC_C_AUTHN_LEVEL_DEFAULT,
+        RPC_C_IMP_LEVEL_IMPERSONATE,
+        NULL, EOAC_NONE, NULL
     );
-
     if (FAILED(hres)) {
         std::cerr << "Failed to initialize security." << std::endl;
         CoUninitialize();
@@ -41,13 +35,11 @@ void RepairDrives() {
 
     // Obtain the initial locator to WMI.
     IWbemLocator* pLoc = NULL;
-
     hres = CoCreateInstance(
-        CLSID_WbemLocator,
-        0,
+        CLSID_WbemLocator, 0,
         CLSCTX_INPROC_SERVER,
-        IID_IWbemLocator, (LPVOID*)&pLoc);
-
+        IID_IWbemLocator, (LPVOID*)&pLoc
+    );
     if (FAILED(hres)) {
         std::cerr << "Failed to create IWbemLocator object." << std::endl;
         CoUninitialize();
@@ -55,19 +47,10 @@ void RepairDrives() {
     }
 
     IWbemServices* pSvc = NULL;
-
-    // Connect to the root\cimv2 namespace with the current user.
     hres = pLoc->ConnectServer(
         _bstr_t(L"ROOT\\CIMV2"),
-        NULL,  // User name
-        NULL,  // User password
-        0,     // Locale
-        NULL,  // Security flags
-        0,     // Authority
-        0,     // Context object
-        &pSvc  // IWbemServices proxy
+        NULL, NULL, 0, NULL, 0, 0, &pSvc
     );
-
     if (FAILED(hres)) {
         std::cerr << "Could not connect to WMI namespace." << std::endl;
         pLoc->Release();
@@ -75,18 +58,12 @@ void RepairDrives() {
         return;
     }
 
-    // Set security levels on the proxy.
     hres = CoSetProxyBlanket(
-        pSvc,                        // Indicates the proxy to set
-        RPC_C_AUTHN_WINNT,           // RPC_C_AUTHN_xxx 
-        RPC_C_AUTHZ_NONE,            // RPC_C_AUTHZ_xxx 
-        NULL,                        // Server principal name 
-        RPC_C_AUTHN_LEVEL_CALL,      // RPC_C_AUTHN_LEVEL_xxx 
-        RPC_C_IMP_LEVEL_IMPERSONATE, // RPC_C_IMP_LEVEL_xxx
-        NULL,                        // client identity
-        EOAC_NONE                    // proxy capabilities 
+        pSvc,
+        RPC_C_AUTHN_WINNT, RPC_C_AUTHZ_NONE, NULL,
+        RPC_C_AUTHN_LEVEL_CALL, RPC_C_IMP_LEVEL_IMPERSONATE,
+        NULL, EOAC_NONE
     );
-
     if (FAILED(hres)) {
         std::cerr << "Could not set proxy blanket." << std::endl;
         pSvc->Release();
@@ -95,15 +72,14 @@ void RepairDrives() {
         return;
     }
 
-    // Get logical disks.
+    // Get logical disks
     IEnumWbemClassObject* pEnumerator = NULL;
     hres = pSvc->ExecQuery(
         bstr_t("WQL"),
         bstr_t("SELECT * FROM Win32_LogicalDisk"),
         WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY,
-        NULL,
-        &pEnumerator);
-
+        NULL, &pEnumerator
+    );
     if (FAILED(hres)) {
         std::cerr << "Query for logical disks failed." << std::endl;
         pSvc->Release();
@@ -112,114 +88,133 @@ void RepairDrives() {
         return;
     }
 
-    IWbemClassObject* pclsObj = NULL;
-    ULONG uReturn = 0;
+    // First pass: Run CHKDSK on all drives
+    {
+        IWbemClassObject* pclsObj = NULL;
+        ULONG uReturn = 0;
 
-    while (pEnumerator) {
-        HRESULT hr = pEnumerator->Next(WBEM_INFINITE, 1, &pclsObj, &uReturn);
+        while (pEnumerator) {
+            HRESULT hr = pEnumerator->Next(WBEM_INFINITE, 1, &pclsObj, &uReturn);
+            if (0 == uReturn) break;
 
-        if (0 == uReturn) {
-            break;
+            VARIANT vtProp;
+            hr = pclsObj->Get(L"DeviceID", 0, &vtProp, 0, 0);
+
+            std::wstring deviceID = vtProp.bstrVal;
+            VariantClear(&vtProp);
+
+            std::wcout << L"Running CHKDSK on " << deviceID << std::endl;
+            std::wstring chkdskCommand = L"chkdsk " + deviceID + L" /f";
+            runCommand(chkdskCommand);
+
+            pclsObj->Release();
         }
 
-        VARIANT vtProp;
-        hr = pclsObj->Get(L"DeviceID", 0, &vtProp, 0, 0);
+        pEnumerator->Release();
+    }
 
-        std::wstring deviceID = vtProp.bstrVal;
-        VariantClear(&vtProp);
-
-        std::wstring query = L"ASSOCIATORS OF {Win32_LogicalDisk.DeviceID='" + deviceID + L"'} WHERE AssocClass=Win32_LogicalDiskToPartition";
-        IEnumWbemClassObject* pPartitionEnum = NULL;
-        hr = pSvc->ExecQuery(
+    // Second pass: Run DEFRAG on eligible drives
+    {
+        // Need to query again
+        hres = pSvc->ExecQuery(
             bstr_t("WQL"),
-            bstr_t(query.c_str()),
+            bstr_t("SELECT * FROM Win32_LogicalDisk"),
             WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY,
-            NULL,
-            &pPartitionEnum);
-
-        if (FAILED(hr)) {
-            std::cerr << "Query for partitions failed." << std::endl;
-            continue;
+            NULL, &pEnumerator
+        );
+        if (FAILED(hres)) {
+            std::cerr << "Query for logical disks failed (second pass)." << std::endl;
+            pSvc->Release();
+            pLoc->Release();
+            CoUninitialize();
+            return;
         }
 
-        IWbemClassObject* pPartitionObj = NULL;
-        ULONG uPartitionReturn = 0;
+        IWbemClassObject* pclsObj = NULL;
+        ULONG uReturn = 0;
 
-        while (pPartitionEnum) {
-            HRESULT hrPartition = pPartitionEnum->Next(WBEM_INFINITE, 1, &pPartitionObj, &uPartitionReturn);
+        while (pEnumerator) {
+            HRESULT hr = pEnumerator->Next(WBEM_INFINITE, 1, &pclsObj, &uReturn);
+            if (0 == uReturn) break;
 
-            if (0 == uPartitionReturn) {
-                break;
-            }
+            VARIANT vtProp;
+            hr = pclsObj->Get(L"DeviceID", 0, &vtProp, 0, 0);
 
-            VARIANT vtPartition;
-            hrPartition = pPartitionObj->Get(L"DeviceID", 0, &vtPartition, 0, 0);
+            std::wstring deviceID = vtProp.bstrVal;
+            VariantClear(&vtProp);
 
-            std::wstring partitionID = vtPartition.bstrVal;
-            VariantClear(&vtPartition);
-
-            std::wstring diskQuery = L"ASSOCIATORS OF {Win32_DiskPartition.DeviceID='" + partitionID + L"'} WHERE AssocClass=Win32_DiskDriveToDiskPartition";
-            IEnumWbemClassObject* pDiskEnum = NULL;
-            hrPartition = pSvc->ExecQuery(
+            // Now fetch associated Disk Model and MediaType
+            std::wstring query = L"ASSOCIATORS OF {Win32_LogicalDisk.DeviceID='" + deviceID + L"'} WHERE AssocClass=Win32_LogicalDiskToPartition";
+            IEnumWbemClassObject* pPartitionEnum = NULL;
+            hres = pSvc->ExecQuery(
                 bstr_t("WQL"),
-                bstr_t(diskQuery.c_str()),
+                bstr_t(query.c_str()),
                 WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY,
-                NULL,
-                &pDiskEnum);
+                NULL, &pPartitionEnum
+            );
 
-            if (FAILED(hrPartition)) {
-                std::cerr << "Query for disks failed." << std::endl;
+            if (FAILED(hres)) {
+                pclsObj->Release();
                 continue;
             }
 
-            IWbemClassObject* pDiskObj = NULL;
-            ULONG uDiskReturn = 0;
+            IWbemClassObject* pPartitionObj = NULL;
+            ULONG uPartitionReturn = 0;
 
-            while (pDiskEnum) {
-                HRESULT hrDisk = pDiskEnum->Next(WBEM_INFINITE, 1, &pDiskObj, &uDiskReturn);
+            HRESULT hrPartition = pPartitionEnum->Next(WBEM_INFINITE, 1, &pPartitionObj, &uPartitionReturn);
+            if (uPartitionReturn) {
+                VARIANT vtPartition;
+                hrPartition = pPartitionObj->Get(L"DeviceID", 0, &vtPartition, 0, 0);
 
-                if (0 == uDiskReturn) {
-                    break;
+                std::wstring partitionID = vtPartition.bstrVal;
+                VariantClear(&vtPartition);
+
+                std::wstring diskQuery = L"ASSOCIATORS OF {Win32_DiskPartition.DeviceID='" + partitionID + L"'} WHERE AssocClass=Win32_DiskDriveToDiskPartition";
+                IEnumWbemClassObject* pDiskEnum = NULL;
+                hres = pSvc->ExecQuery(
+                    bstr_t("WQL"),
+                    bstr_t(diskQuery.c_str()),
+                    WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY,
+                    NULL, &pDiskEnum
+                );
+
+                if (SUCCEEDED(hres)) {
+                    IWbemClassObject* pDiskObj = NULL;
+                    ULONG uDiskReturn = 0;
+                    HRESULT hrDisk = pDiskEnum->Next(WBEM_INFINITE, 1, &pDiskObj, &uDiskReturn);
+                    if (uDiskReturn) {
+                        VARIANT vtModel, vtMediaType;
+                        hrDisk = pDiskObj->Get(L"Model", 0, &vtModel, 0, 0);
+                        hrDisk = pDiskObj->Get(L"MediaType", 0, &vtMediaType, 0, 0);
+
+                        std::wstring model = vtModel.bstrVal;
+                        std::wstring mediaType = vtMediaType.bstrVal;
+                        VariantClear(&vtModel);
+                        VariantClear(&vtMediaType);
+
+                        if (model != L"Red Hat VirtIO SCSI Disk Device" && mediaType != L"SSD") {
+                            std::wcout << L"Running DEFRAG on " << deviceID << std::endl;
+                            std::wstring defragCommand = L"defrag " + deviceID + L"\\ /O";
+
+                            runCommand(defragCommand);
+                        }
+                        else {
+                            std::wcout << L"Skipping DEFRAG on " << deviceID << L" - Disk model: " << model << std::endl;
+                        }
+
+                        pDiskObj->Release();
+                    }
+                    pDiskEnum->Release();
                 }
-
-                VARIANT vtModel, vtMediaType;
-                hrDisk = pDiskObj->Get(L"Model", 0, &vtModel, 0, 0);
-                hrDisk = pDiskObj->Get(L"MediaType", 0, &vtMediaType, 0, 0);
-
-                std::wstring model = vtModel.bstrVal;
-                std::wstring mediaType = vtMediaType.bstrVal;
-                VariantClear(&vtModel);
-                VariantClear(&vtMediaType);
-
-                if (model != L"Red Hat VirtIO SCSI Disk Device" && mediaType != L"SSD") {
-                    std::wstring driveLetter = deviceID;
-                    std::wcout << L"Running maintenance on drive " << driveLetter << std::endl;
-
-                    std::wstring chkdskCommand = L"chkdsk " + driveLetter + L" /f";
-                    std::wstring defragCommandL = L"defrag " + driveLetter + L"\\ /L";
-                    std::wstring defragCommandO = L"defrag " + driveLetter + L"\\ /O";
-
-                    runCommand(chkdskCommand);
-                    runCommand(defragCommandL);
-                    runCommand(defragCommandO);
-                }
-                else {
-                    std::wcout << L"Skipping drive " << deviceID << L" - Disk model: " << model << std::endl;
-                }
-
-                pDiskObj->Release();
+                pPartitionObj->Release();
             }
-
-            pDiskEnum->Release();
-            pPartitionObj->Release();
+            pPartitionEnum->Release();
+            pclsObj->Release();
         }
-
-        pPartitionEnum->Release();
-        pclsObj->Release();
+        pEnumerator->Release();
     }
 
     pSvc->Release();
     pLoc->Release();
-    pEnumerator->Release();
     CoUninitialize();
 }
